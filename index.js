bfc61fa0952f00647a63f59f72d46e25d1a4c8f6
Coding_girl1 <42157883+aidansoffe@users.noreply.github.com>
2020-02-26 23:09:29 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 function dwarfRollCall(dwarves) {
+  var newString = ''
+  for(let i = 0; i < dwarves.length; i++){
+     newString +=  `${i+1}. ${dwarves[i]} `
+  }
+   return newString
 }
+
+
+
 
 function summonCaptainPlanet(planeteerCalls){
+planeteerCalls = planeteerCalls.map(function(each) {
+  return each.toUpperCase()+'!'
+});
+return planeteerCalls
 }
+
+
+
 
 function longPlaneteerCalls(words) {
-}
+  if(words.length >= 4){
+    return true
+  }else {
+    return false
+  }
 
-function findTheCheese (foods) {
 }
